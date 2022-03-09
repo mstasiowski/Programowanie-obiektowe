8663b608d79fbaea915e580eb61eba5074ccc13f
michal.stasiowski <michal.stasiowski@PK3-16>
2022-03-09 10:27:28 +0100

    llab

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -21,53 +21,61 @@ namespace lab_1
             string name1 = "adam";
             string v = name.Substring(0, 2);
             Console.WriteLine(name1 == name);
+
+            Money money = Money.Of(10, Currency.PLN);
+            //money * 5 -> *(money, 5)
+            Money result = money * 5;
+
+            Console.WriteLine(result.Value);
+
         }
 
-                    class Person
+        class Person
+        {
+
+            private Person(string name)
+            {
+                _Name = name;
+            }
+
+            public static Person Of(string name)
+            {
+
+                if (name != null && name.Length >= 2)
+                {
+                    return new Person(name);
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Imię zbyt krótkie!");
+                }
+
+            }
+
+            private string _Name;
+            public string Name
+            {
+                get
+                {
+                    return _Name;
+                }
+
+                set
+                {
+                    if (value != null && value.Length >= 2)
                     {
-            
-                        private Person(string name)
-                        {
-                            _Name = name;
-                        }
-            
-                        public static  Person Of(string name)
-                        {
-
-                            if(name !=null && name.Length >= 2)
-                            {
-                                return new Person(name);
-                            }
-                            else
-                            {
-                                throw new ArgumentOutOfRangeException("Imię zbyt krótkie!");
-                            }
-
-                        }
-            
-                        private string _Name;
-                        public string Name
-                        {
-                            get
-                            {
-                                return _Name;
-                            }
-
-                            set
-                            {
-                                if(value != null  && value.Length >= 2)
-                                {
-                                    _Name = value;
-                                }else
-                                {
-                                    throw new ArgumentOutOfRangeException("Imię zbyt krótkie !");
-                                }
-                            }
-                        }
+                        _Name = value;
                     }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Imię zbyt krótkie !");
+                    }
+                }
+            }
+        }
 
-    
-    public enum Currency
+
+        public enum Currency
         {
             PLN = 1,
             USD = 2,
@@ -84,13 +92,35 @@ namespace lab_1
                 _currency = currency;
             }
 
+            public decimal Value
+            {
+                get
+                {
+                    return _value;
+                }
+            }
+
+            public Currency Currency
+            {
+                get
+                {
+                    return _currency;
+                }
+            }
+
+
+
 
             public static Money? Of(decimal value, Currency currency)
             {
                 return value < 0 ? null : new Money(value, currency);
             }
 
-
+            public static Money operator *(Money a, decimal b)
+            {
+                return Money.Of(a._value * b, a._currency);
+            }
+            //aby mnożyc 5* money musimy ten public operator nowy napisać
 
         }
 
