cd3e780d510ef24a15b2e0c8cd493478b4a20678
mstasiowski <mstasiowski787@gmail.com>
2022-03-30 10:18:20 +0200

    aa

---
+++ lab-3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace lab_3
+{
+    class Stack<T> /* where T: Student */
+    {
+        private  T[] arr = new T[10]; //string
+        private int _last = -1;
+
+        public void Push(T item) //Push(string item)
+        {
+             arr[++_last] = item;
+
+        }
+
+        public T Pop()
+            {
+
+            return arr[_last--];
+        }
+
+    }
+
+    class Student
+    {
+        private string _firstname;
+        public int Egzam;
+
+        public void Push(Stack<string> stack)
+        {
+            stack.Push(_firstname);
+        }
+
+        public T GetReward<T>(T reward)
+        {
+           if(Egzam > 50)
+            {
+                return reward;
+            }else
+            {
+                return default;
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student() {Egzam = 60 };
+            string v = student.GetReward("Gratulacje");
+            Console.WriteLine(v);
+            ValueTuple<string, decimal, int> product = ValueTuple.Create("Laptop", 1200m, 2);
+            Console.WriteLine(product);
+            (string, decimal, int) laptop = ("laptop", 1200m, 2);
+            Console.WriteLine(product == laptop);
+            (string name, decimal price, int quantity ) tuple = ("laptop", 3000m, 2);
+            laptop = tuple;
+            Console.WriteLine(laptop);
+
+            var tuple1 = (name: "laptop", price: 1299m);
+        }
+    }
+}
---
+++ lab-4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace lab_4
+{
+    enum Degree
+    {
+        A = 50,
+        B=45,
+        C=40,
+        D=35,
+        E=30,
+        F=20
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Degree studentDegree = Degree.C;
+            Console.WriteLine((int)studentDegree);
+            foreach(string name in Enum.GetNames<Degree>())
+            {
+                Console.WriteLine(name);
+            }
+            foreach (Degree degree in Enum.GetValues<Degree>())
+            {
+                Console.WriteLine($"{degree}{(int)degree }");
+            }
+
+            Console.WriteLine("Wpisz oceny");
+            string str = Console.ReadLine();
+            studentDegree = Enum.Parse<Degree>(str);
+            try
+            {
+                studentDegree = Enum.Parse<Degree>(str);
+            Console.WriteLine($"Wpisałeś ocene {studentDegree} {(int)studentDegree}");
+
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine("Nieznana ocena!");
+            }
+
+
+        }
+    }
+}
